6e485b6782cdd50ff028179d82312346bd0dbd14
namgyeonghwa <ska2870ghk@naver.com>
2022-01-18 04:25:24 +0900

    LongClickListener & PopupMenu
    when an item of recycler view is selected,
    then apopup menu appeared.
    not yet implemented: custom

--- ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainActivity.kt
+++ ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainActivity.kt
@@ -8,9 +8,13 @@ import com.chat_soon_e.chat_soon_e.databinding.ActivityMainBinding
 import com.chat_soon_e.chat_soon_e.ui.BaseActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.R
 import android.content.Intent
+import android.view.MenuItem
+import android.view.View
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.core.app.NotificationManagerCompat
+import com.chat_soon_e.chat_soon_e.R
 
 class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     private lateinit var chatDB: AppDatabase            // chat list를 담고 있는 데이터베이스
@@ -51,13 +55,13 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
         // 이미 데이터가 있다는 것을 뜻하므로 함수를 리턴한다.
         if(chatList.isNotEmpty()) return
 
-        // 만약 데이터베이스에서 받아온 chat list가 비어있는 경우
-        // 더미 데이터
-        // chatDB.chatDao().insert(Chat(null, "강은서", "Designer", "오전 10시"))
-        // chatDB.chatDao().insert(Chat(null, "김민경", "PM\nServer(node.js)", "오전 11시"))
-        // chatDB.chatDao().insert(Chat(null, "남선우", "Android", "오전 12시"))
-        // chatDB.chatDao().insert(Chat(null, "변재호", "Server (node.js)", "오후 1시"))
-        // chatDB.chatDao().insert(Chat(null, "이주연", "Android", "오후 2시"))
+         // 만약 데이터베이스에서 받아온 chat list가 비어있는 경우
+         // 더미 데이터
+         chatDB.chatDao().insert(Chat(null, "강은서", "Designer", "오전 10시"))
+         chatDB.chatDao().insert(Chat(null, "김민경", "PM\nServer(node.js)", "오전 11시"))
+         chatDB.chatDao().insert(Chat(null, "남선우", "Android", "오전 12시"))
+         chatDB.chatDao().insert(Chat(null, "변재호", "Server (node.js)", "오후 1시"))
+         chatDB.chatDao().insert(Chat(null, "이주연", "Android", "오후 2시"))
     }
 
     private fun initRecyclerView() {
@@ -67,11 +71,29 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
             DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(dividerItemDecoration)
 
-        // 어댑터 연결
+        // 더미 데이터와 어댑터 연결
         chatDB = AppDatabase.getInstance(this)!!
         chatList = chatDB.chatDao().getChatList() as ArrayList
         mainRVAdapter = MainRVAdapter(chatList)
         binding.mainChatListRecyclerView.adapter = mainRVAdapter
+
+        // 아이템 뷰가 길게 클릭됐을 때 팝업 메뉴 띄우기
+        mainRVAdapter.setMyItemClickListener(object: MainRVAdapter.MyItemClickListener {
+            override fun onChatLongClick(view: View, position: Int) {
+                // 팝업 메뉴 나오도록
+                // PopupMenu는 API 11 레벨부터 제공된다.
+                Log.d("MAIN/LONG-CLICK", "onChatLongClick")
+
+                // 여기서 view는 클릭된 뷰를 의미한다.
+                var popup = PopupMenu(this@MainActivity, view)
+                menuInflater.inflate(R.menu.popup_menu, popup.menu)
+
+                // 리스너로 처리
+                var listener = PopupMenuListener()
+                popup.setOnMenuItemClickListener(listener)
+                popup.show()    // 팝업 메뉴 보이도록
+            }
+        })
     }
 
     private fun initClickListener() {
@@ -95,6 +117,21 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
     private fun permissionGrantred(): Boolean {
         return NotificationManagerCompat.getEnabledListenerPackages(this).any {
             enabledPackageName -> enabledPackageName == packageName
+        }
+    }
+
+    // 팝업 메뉴 리스너
+    inner class PopupMenuListener: PopupMenu.OnMenuItemClickListener {
+        override fun onMenuItemClick(item: MenuItem?): Boolean {
+            when(item?.itemId) {
+                R.id.popup_menu_1
+                        -> Toast.makeText(this@MainActivity, "삭제하기", Toast.LENGTH_SHORT).show()
+                R.id.popup_menu_2
+                        -> Toast.makeText(this@MainActivity, "차단하기", Toast.LENGTH_SHORT).show()
+                else
+                    -> Toast.makeText(this@MainActivity, "잘못된 선택입니다.", Toast.LENGTH_SHORT).show()
+            }
+            return false
         }
     }
 }
--- ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainRVAdapter.kt
+++ ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainRVAdapter.kt
@@ -1,6 +1,8 @@
 package com.chat_soon_e.chat_soon_e.ui
 
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
@@ -9,31 +11,47 @@ import com.chat_soon_e.chat_soon_e.databinding.ItemChatListBinding
 
 class MainRVAdapter(private val chatList: ArrayList<Chat>): RecyclerView.Adapter<MainRVAdapter.ViewHolder>() {
 
+    // 클릭 인터페이스
     interface MyItemClickListener {
-        fun onChatClick(chatIdx: Int)
+        fun onChatLongClick(view: View, position: Int)
     }
 
+    // 리스너 객체를 저장하는 변수
     private lateinit var mItemClickListener: MyItemClickListener
 
+    // 리스너 객체를 외부에서 전달받는 함수
     fun setMyItemClickListener(itemClickListener: MyItemClickListener) {
         mItemClickListener = itemClickListener
     }
 
+    // 뷰홀더를 생성해줘야 할 때 호출되는 함수로, 아이템 뷰 객체를 만들어서 뷰 홀더에 던져준다.
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding: ItemChatListBinding = ItemChatListBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(binding)
     }
 
+    // 뷰홀더에 데이터 바인딩을 해줘야 할 때마다 호출되는 함수
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(chatList[position])
-        holder.itemView.setOnClickListener {
-            mItemClickListener.onChatClick(chatList[position].idx)
+        holder.itemView.setOnLongClickListener {
+            mItemClickListener.onChatLongClick(holder.itemView, position)
+            return@setOnLongClickListener true
         }
     }
 
+    // 데이터셋의 크기를 알려주는 함수
     override fun getItemCount(): Int = chatList.size
 
+    // 뷰홀더
     inner class ViewHolder(private val binding: ItemChatListBinding): RecyclerView.ViewHolder(binding.root) {
+//        init {
+//            itemView.setOnLongClickListener {
+//                mItemClickListener.onChatLongClick(this, adapterPosition)
+//                return@setOnLongClickListener true
+//            }
+//        }
+
+        // 뷰 객체가 담겨있는 binding에 데이터 리스트에서 받아온 데이터들을 넣어준다.
         fun bind(chat: Chat) {
             // 프로필 사진 어떻게 처리할 건지
             binding.itemChatListNameTv.text = chat.name
