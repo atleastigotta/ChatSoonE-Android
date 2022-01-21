8f461738f706545cdace3288a043d2009ad7868a
namgyeonghwa <ska2870ghk@naver.com>
2022-01-22 00:47:11 +0900

    Design of chat list applied

--- ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainActivity.kt
+++ ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainActivity.kt
@@ -10,35 +10,49 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.PopupMenu
 import android.widget.Toast
+import android.widget.Toolbar
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.view.GravityCompat
 import com.chat_soon_e.chat_soon_e.R
+import com.chat_soon_e.chat_soon_e.databinding.ActivityMainContentBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate), NavigationView.OnNavigationItemSelectedListener {
+//    private lateinit var contentBinding: ActivityMainBinding
     private lateinit var chatDB: AppDatabase            // chat list를 담고 있는 데이터베이스
     private lateinit var mainRVAdapter: MainRVAdapter   // chat list recycler view adpater
     private var chatList = ArrayList<Chat>()            // 데이터베이스로부터 chat list를 받아올 변수
+
+    override fun initAfterBinding() {
+        Log.d("MAIN/LIFE-CYCLE", "after onCreate()")
+    }
 
     override fun onStart() {
         super.onStart()
         Log.d("MAIN/LIFE-CYCLE", "onStart()")
 
-        initAlarmStack()
-        initChatList()
-        initRecyclerView()
-        initClickListener()
-    }
-
-    override fun initAfterBinding() {
+        initNotificationListener()  // NotificationListener (알림 권한 허용)
+        initChatList()              // chat list 데이터 초기화
+        initRecyclerView()          // RecylcerView Adapter 연결 & 기타 설정
+        initDrawerLayout()          // 설정 메뉴창 설정
+        initClickListener()         // 여러 ClickListener 초기화
     }
 
     // NotificationListener 실행
-    private fun initAlarmStack() {
+    private fun initNotificationListener() {
         // 권한 허용이 안 되어 있다면 권한 허용을 하는 action을 시작한다.
         if(!permissionGrantred()) {
             val intent = Intent(
                 "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"
             )
             startActivity(intent)
+        }
+    }
+
+    // 권한 체크
+    private fun permissionGrantred(): Boolean {
+        return NotificationManagerCompat.getEnabledListenerPackages(this).any {
+                enabledPackageName -> enabledPackageName == packageName
         }
     }
 
@@ -52,18 +66,23 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
         // 이미 데이터가 있다는 것을 뜻하므로 함수를 리턴한다.
         if(chatList.isNotEmpty()) return
 
-         // 만약 데이터베이스에서 받아온 chat list가 비어있는 경우
-         // 더미 데이터
-         chatDB.chatDao().insert(Chat(null, "강은서", "Designer", "오전 10시"))
-         chatDB.chatDao().insert(Chat(null, "김민경", "PM\nServer(node.js)", "오전 11시"))
-         chatDB.chatDao().insert(Chat(null, "남선우", "Android", "오전 12시"))
-         chatDB.chatDao().insert(Chat(null, "변재호", "Server (node.js)", "오후 1시"))
-         chatDB.chatDao().insert(Chat(null, "이주연", "Android", "오후 2시"))
+        // 만약 데이터베이스에서 받아온 chat list가 비어있는 경우
+        // 더미 데이터
+        chatDB.chatDao().insert(Chat(null, "강은서", "Designer", "오전 10시"))
+        chatDB.chatDao().insert(Chat(null, "김민경", "PM\nServer(node.js)", "오전 11시"))
+        chatDB.chatDao().insert(Chat(null, "남선우", "Android", "오전 12시"))
+        chatDB.chatDao().insert(Chat(null, "변재호", "Server (node.js)", "오후 1시"))
+        chatDB.chatDao().insert(Chat(null, "이주연", "Android", "오후 2시"))
+
+        Log.d("MAIN/DATA", chatDB.chatDao().getChatList().toString())
     }
 
+    // RecyclerView
     private fun initRecyclerView() {
+//        val binding = ActivityMainContentBinding.inflate(layoutInflater)
+
         // RecyclerView 구분선
-        val recyclerView = binding.mainChatListRecyclerView
+        val recyclerView = binding.mainContent.mainChatListRecyclerView
         val dividerItemDecoration =
             DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
         recyclerView.addItemDecoration(dividerItemDecoration)
@@ -72,10 +91,14 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
         chatDB = AppDatabase.getInstance(this)!!
         chatList = chatDB.chatDao().getChatList() as ArrayList
         mainRVAdapter = MainRVAdapter(chatList)
-        binding.mainChatListRecyclerView.adapter = mainRVAdapter
+        binding.mainContent.mainChatListRecyclerView.adapter = mainRVAdapter
 
         // 아이템 뷰가 길게 클릭됐을 때 팝업 메뉴 띄우기
         mainRVAdapter.setMyItemClickListener(object: MainRVAdapter.MyItemClickListener {
+            override fun onChatClick(view: View, position: Int) {
+                TODO("Not yet implemented")
+            }
+
             override fun onChatLongClick(view: View, position: Int) {
                 // 팝업 메뉴 나오도록
                 // PopupMenu는 API 11 레벨부터 제공된다.
@@ -93,28 +116,55 @@ class MainActivity: BaseActivity<ActivityMainBinding>(ActivityMainBinding::infla
         })
     }
 
+    // 설정 메뉴 창을 띄우는 DrawerLayout 초기화
+    private fun initDrawerLayout() {
+//        드로어를 꺼낼 홈버튼 활성화
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        홈버튼 이미지 변경
+//        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+        binding.mainNavigationView.setNavigationItemSelectedListener(this)
+    }
+
+//    툴바 메뉴 버튼이 클릭되었을 때 실행된다.
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId) { // 메뉴 버튼
+//            android.R.id.home ->
+//                binding.mainDrawerLayout.openDrawer(GravityCompat.START)
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+
+    // 설정 메뉴 창의 네비게이션 드로어의 아이템들에 대한 이벤트를 처리
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.navi_setting_alarm_item -> Toast.makeText(this, "alarm clicked", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    // 드로어가 나와있을 때 뒤로 가기 버튼을 한 경우 뒤로 가기 버튼에 대한 이벤트를 처리
+    override fun onBackPressed() {
+        if(binding.mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.mainDrawerLayout.closeDrawers()
+            Toast.makeText(this, "뒤로 가기", Toast.LENGTH_SHORT).show()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     private fun initClickListener() {
-        // 내폴더 아이콘을 클릭했을 때 폴더 화면으로 이동할 수 있도록
-        binding.mainMyFolderIv.setOnClickListener {
-            startNextActivity(FolderActivity::class.java)
-        }
+//        val binding = ActivityMainContentBinding.inflate(layoutInflater)
 
-        // 업데이트 버튼을 클릭했을 때 업데이트 해주는 함수 실행
-        binding.mainUpdateIv.setOnClickListener {
-            update()
-        }
-    }
-
-    // 알림 스택을 확인하고, 새로 추가된 내용이 있으면 업데이트해주는 함수
-    private fun update() {
-        initAlarmStack()
-    }
-
-    // 권한 체크
-    private fun permissionGrantred(): Boolean {
-        return NotificationManagerCompat.getEnabledListenerPackages(this).any {
-            enabledPackageName -> enabledPackageName == packageName
-        }
+//        내폴더 아이콘을 클릭했을 때 폴더 화면으로 이동할 수 있도록
+//        binding.mainMyFolderIv.setOnClickListener {
+//            startNextActivity(FolderActivity::class.java)
+//        }
+//
+//        업데이트 버튼을 클릭했을 때 업데이트 해주는 함수 실행
+//        binding.mainUpdateIv.setOnClickListener {
+//            update()
+//        }
     }
 
     // 팝업 메뉴 리스너
--- ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainRVAdapter.kt
+++ ChatSoonE/app/src/main/java/com/chat_soon_e/chat_soon_e/ui/MainRVAdapter.kt
@@ -1,7 +1,5 @@
 package com.chat_soon_e.chat_soon_e.ui
 
-import android.graphics.Color
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +8,19 @@ import com.chat_soon_e.chat_soon_e.data.entities.Chat
 import com.chat_soon_e.chat_soon_e.databinding.ItemChatListBinding
 
 class MainRVAdapter(private val chatList: ArrayList<Chat>): RecyclerView.Adapter<MainRVAdapter.ViewHolder>() {
-    // 현재 선택된 position을 저장할 변수
-    // 현재 position과 일치할 경우 배경을 바꾸고, 그 외에는 하얀색 (default) 처리
-    var selectedPosition = -1
+//    현재 선택된 position을 저장할 변수
+//    현재 position과 일치할 경우 배경을 바꾸고, 그 외에는 하얀색 (default) 처리
+//    var selectedPosition = -1
+
+    // 선택한 리스트 아이템의 position 번호를 저장
+    private var selectedPosition = arrayListOf<Long>()
+
+    // selectedPosition의 아이템 정보를 출력하기 위한 람다 함수
+    var onItemSelectionChangedListener: ((MutableList<Long>) -> Unit)? = null
 
     // 클릭 인터페이스
     interface MyItemClickListener {
+        fun onChatClick(view: View, position: Int)
         fun onChatLongClick(view: View, position: Int)
     }
 
@@ -37,24 +42,25 @@ class MainRVAdapter(private val chatList: ArrayList<Chat>): RecyclerView.Adapter
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(chatList[position])
 
-        // RecylcerView 커스텀
-        // 클릭된 아이템 뷰일 경우 와이어 프레임에 나온 것처럼 배경 회색 처리
-        if(selectedPosition == position) {
-            holder.itemView.setBackgroundColor(Color.LTGRAY)
-        } else {
-            holder.itemView.setBackgroundColor(Color.WHITE)
-        }
+//        RecylcerView 커스텀
+//        클릭된 아이템 뷰일 경우 와이어 프레임에 나온 것처럼 배경 회색 처리
+//        if(selectedPosition == position) {
+//            holder.itemView.setBackgroundColor(Color.LTGRAY)
+//        } else {
+//            holder.itemView.setBackgroundColor(Color.WHITE)
+//        }
 
         holder.itemView.setOnLongClickListener {
-            // 아이템 뷰의 항목을 길게 클릭했을 때 selectPosition을 beforePosition에 저장한 후
-            // 현재 position으로 바꿔준다.
-            val beforePosition = selectedPosition
-            selectedPosition = position
+//            아이템 뷰의 항목을 길게 클릭했을 때 selectPosition을 beforePosition에 저장한 후
+//            현재 position으로 바꿔준다.
+//            val beforePosition = selectedPosition
+//            selectedPosition = position
 
-            // beforePosition, selectPosition을 전달해줌으로써 해당 항목들의 레이아웃이 다시 생성되고, 스타일이 업데이트 된다.
-            notifyItemChanged(beforePosition)
-            notifyItemChanged(selectedPosition)
-            // notifyDataSetChanged()
+//            beforePosition, selectPosition을 전달해줌으로써 해당 항목들의 레이아웃이 다시 생성되고, 스타일이 업데이트 된다.
+//            notifyItemChanged(beforePosition)
+//            notifyItemChanged(selectedPosition)
+//            notifyDataSetChanged()
+
 
             mItemClickListener.onChatLongClick(holder.binding.itemChatListDateTimeTv, position)
             return@setOnLongClickListener true
@@ -63,6 +69,9 @@ class MainRVAdapter(private val chatList: ArrayList<Chat>): RecyclerView.Adapter
 
     // 데이터셋의 크기를 알려주는 함수
     override fun getItemCount(): Int = chatList.size
+
+    // 아이템 아이디 반환
+    override fun getItemId(position: Int): Long = position.toLong()
 
     // 뷰홀더
     inner class ViewHolder(val binding: ItemChatListBinding): RecyclerView.ViewHolder(binding.root) {
